71dfb34316410fe3e52a12efe53c9394a1d4947d
Filipecarmoss <filipe.carmoss@hotmail.com>
2021-02-27 13:29:32 -0300

    Fragments

---
+++ app/src/main/java/com/br/digitalhouseaulas/FragmentExample2.kt
@@ -0,0 +1,19 @@
+package com.br.digitalhouseaulas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentExample2:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_example2, container, false)
+    }
+
+}
